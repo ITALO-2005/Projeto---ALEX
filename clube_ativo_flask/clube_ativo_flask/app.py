437cf441e7a7a437f0d7416014cebf1fd2042cf0
PauloVitor007 <paulo.hermogens@academico.ifpb.edu.br>
2025-07-11 19:16:03 +0000

    l

--- clube_ativo_flask/clube_ativo_flask/app.py
+++ clube_ativo_flask/clube_ativo_flask/app.py
@@ -1,4 +1,5 @@
-import os Cu preto
+ITALO DANTAS
+import os
 from functools import wraps
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
